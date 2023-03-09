8279bf7a659f09fccdf071ce1fad6100b66cdea9
Tim Ferguson <FergusonTG@gmail.com>
2023-03-09 11:14:28 +0000

    Update rasdf.bash

--- rasdf.bash
+++ rasdf.bash
@@ -1,23 +1,31 @@
 # pass command to rasdf; but remove the history number on the way
 export PROMPT_COMMAND='rasdf add $PWD $(history 1 | sed -e '"'"'s/^\s*\(\S\+\s\+\)//'"'"')'
 
-# Case-insensitive and lax matching
-export RASDF_FLAGS='il'
+# Case-insensitive but strict matching
+export RASDF_FLAGS='is'
 
 z() { 
   local newdir
   newdir=$( rasdf find -dsi $@ )
   if [ -n "$newdir" ]; then 
+    printf "%s\n" "$newdir" >&2
     cd "$newdir"
   else
+    printf "Can't find matching directory\n" >&2
     return 2
   fi 
 }
 
-alias a='rasdf find -aD '
+alias a='rasdf find -Dacl '
 alias s='rasdf find-all -ail '
-alias d='rasdf find -d '
-alias f='rasdf find -f '
-alias v='nvim $( f $@ )'
+alias d='rasdf find -dsi '
+alias f='rasdf find -fsi '
+function v {
+  filename="$( rasdf find -fsi $* )"
+  if [ -f "$filename" ] 
+    then nvim "$filename"
+    else echo cannot find "$filename" >&2
+  fi
+}
 
 rasdf clean
